d817915552e008833819d6063721681f08c486b4
Dmitrii <drozdovdm27@gmail.com>
2022-01-14 00:15:19 +0300

    include to last commit

---
+++ UltraHyperOpenConference/ViewModels/SearchMessageByKeyword.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UltraHyperOpenConference.Model;
+
+namespace UltraHyperOpenConference.ViewModels
+{
+    public class SearchMessageByKeyword
+    {
+        public string Keyword { get; set; }
+        public List<MessageWithUserName> Result { get; set; }
+    }
+}
